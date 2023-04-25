a1cdf50d7132df4e02525ad8c7e8c788782624d5
nicolo.depietro <nicolo.depietro@issgreppi.it>
2023-04-25 16:59:09 +0200

    Aggiungere i file di progetto.

---
+++ ProgettoCinema_DePietroNicolò2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProgettoCinema_DePietroNicolò2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
