515e3bccdb6ef7159f341992531e8555a57cdb64
nicolo.depietro <nicolo.depietro@issgreppi.it>
2023-04-26 17:59:41 +0200

    aggiunta di Create

--- ProgettoCinema_DePietroNicolò2/Controllers/UtenteController.cs
+++ ProgettoCinema_DePietroNicolò2/Controllers/UtenteController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProgettoCinema_DePietroNicolò2.Data;
+using ProgettoCinema_DePietroNicolò2.Models;
 
 namespace ProgettoCinema_DePietroNicolò2.Controllers
 {
     public class UtenteController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public UtenteController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET
         public IActionResult Index()
+        {
+            IEnumerable<Utente> objCategoryList = _db.Utenti;
+            return View(objCategoryList);
+        }
+        //GET
+        public IActionResult Create()
         {
             return View();
         }
     }
+
 }
+
